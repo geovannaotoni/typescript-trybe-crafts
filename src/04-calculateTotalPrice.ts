a935184d28b0ec6e0f414d6ebb5f901e7d0b3b16
Geovanna Otoni <geovannaotoni@gmail.com>
2023-09-06 14:44:56 -0300

    feat: calculateTotalPrice

--- src/04-calculateTotalPrice.ts
+++ src/04-calculateTotalPrice.ts
@@ -1,3 +1,13 @@
-// export default function calculateTotalPrice() {
-//   // insira a solução
-// }
+import { Product } from './02-insertProducts';
+
+export type Item = {
+  product: Product
+  quantity: number
+  phrase: string
+  brand: string
+};
+
+export default function calculateTotalPrice(itens: Item[]): number {
+  const total = itens.reduce((acc, item) => acc + item.quantity * item.product.price, 0);
+  return Number(total.toFixed(2));
+}
