c94f3de57d842274167587c5db2c170f161e5cfd
Geovanna Otoni <geovannaotoni@gmail.com>
2023-09-06 14:39:28 -0300

    feat: findById

--- src/03-findById.ts
+++ src/03-findById.ts
@@ -1,27 +1,29 @@
-// const products = [
-//   {
-//     id: 1,
-//     name: 'Camiseta',
-//     price: 29.9,
-//     quantity: 3,
-//     brands: ['Nike', 'Reebok'],
-//   },
-//   {
-//     id: 2,
-//     name: 'Caneca',
-//     price: 19.90,
-//     quantity: 12,
-//     brands: ['Stanley', 'Camelbak'],
-//   },
-//   {
-//     id: 3,
-//     name: 'Boné',
-//     price: 39.90,
-//     quantity: 0,
-//     brands: [],
-//   },
-// ];
+import { Product } from './02-insertProducts';
 
-// export default function findById() {
-//   // insira a solução
-// }
+const products = [
+  {
+    id: 1,
+    name: 'Camiseta',
+    price: 29.9,
+    quantity: 3,
+    brands: ['Nike', 'Reebok'],
+  },
+  {
+    id: 2,
+    name: 'Caneca',
+    price: 19.90,
+    quantity: 12,
+    brands: ['Stanley', 'Camelbak'],
+  },
+  {
+    id: 3,
+    name: 'Boné',
+    price: 39.90,
+    quantity: 0,
+    brands: [],
+  },
+];
+
+export default function findById(id: number): Product | string {
+  return products.find((product) => product.id === id) || `Produto com ID ${id} não encontrado.`;
+}
