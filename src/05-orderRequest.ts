85e7dcda7dfac7324e4592cb70e245d2a4ab48b5
Geovanna Otoni <geovannaotoni@gmail.com>
2023-09-06 14:56:15 -0300

    feat: orderRequest

--- src/05-orderRequest.ts
+++ src/05-orderRequest.ts
@@ -1,3 +1,17 @@
-// export default function orderRequest() {
-//   // insira a solução
-// }
+import { Item } from './04-calculateTotalPrice';
+
+export type Order = {
+  id: number;
+  customerName: string;
+  customerEmail: string;
+  items: Item[];
+  status: 'pendente' | 'enviado' | 'entregue';
+};
+
+export default function orderRequest(order: Order): string {
+  const prodWithoutStock = order.items.find((item) => item.product.quantity === 0);
+  if (prodWithoutStock) {
+    throw new Error(`Desculpe, ${prodWithoutStock.product.name} não está disponível no estoque`);
+  }
+  return `Olá ${order.customerName}, o seu pedido de ID ${order.id} foi enviado.`;
+}
